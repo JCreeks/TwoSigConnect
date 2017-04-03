e19a2e5cc344fb080bb136e39d7e2cc90ec3ec53
jing guo <jingguo@jings-MacBook-Air.local>
2017-04-03 07:32:12 -0400

    apl 3

--- XGBoostPackage.py
+++ XGBoostPackage.py
@@ -82,7 +82,9 @@ def runXGBShuffle(X_train, y_train, X_test, num_class=1, feature_names=None, see
 # In[43]:
 
 class xgbClass(object):
-    def __init__(self, eta=.1, subsample=.8, num_class=1, max_depth=5, seed=17, silent=0, eva_metric='mlogloss',                colsample_bytree=1, objective='solfprob', min_child_weight=1):
+    def __del__(self):
+        return 
+    def __init__(self, eta=.1, subsample=.8, num_class=1, max_depth=5, seed=17, silent=0, eva_metric='mlogloss',                colsample_bytree=1, objective='solfprob', min_child_weight=1, num_rounds=500):
         self.params={
         'objective' : objective, #'reg:linear','multi:softprob'
         'subsample' : subsample,
@@ -94,14 +96,15 @@ class xgbClass(object):
         'eval_metric': eva_metric, #'rmse' "logloss", "mlogloss", auc" # for ranking
         'min_child_weight': min_child_weight
         }
+        self.num_rounds=num_rounds
         
         if num_class!=1:
             self.params['num_class']=num_class
         self.model=[]
         
-    def fit(self, X_train, y_train, num_rounds=500, early_stopping_rounds=None):
+    def fit(self, X_train, y_train, early_stopping_rounds=None):
         dtrain = xgb.DMatrix(X_train, label=y_train)
-        self.model = xgb.train(self.params, dtrain, num_boost_round=num_rounds, early_stopping_rounds=early_stopping_rounds)
+        self.model = xgb.train(self.params, dtrain, num_boost_round=self.num_rounds, early_stopping_rounds=early_stopping_rounds)
     
     def predict(self, X_test):
         dtest = xgb.DMatrix(X_test)
